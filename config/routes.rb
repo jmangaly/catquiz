77887de16bd7d2c297dae1eb5f1315ca972225ae
Jenny Mangaly <jmangaly@gmail.com>
2015-12-03 22:15:31 +0000

    Create cats

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Catquiz::Application.routes.draw do
+  root 'cats#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
